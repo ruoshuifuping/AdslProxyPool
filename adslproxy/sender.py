6f78ec737f51e0d5933f966ae805355d37c2c086
ruoshuifuping <zhao2000fu@163.com>
2018-01-19 10:06:29 +0800

    Update sender.py

--- adslproxy/sender.py
+++ adslproxy/sender.py
@@ -30,7 +30,7 @@ class Sender():
 
     def test_proxy(self, proxy):
         try:
-            if proxy != self.proxy:
+            if proxy not in self.proxy:
                 print('start texting')
                 proxies = {'http':'http://'+proxy}
                 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
