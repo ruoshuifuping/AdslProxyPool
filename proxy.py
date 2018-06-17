3b4c1ec38e4725536bb11ec04ec0624282e166c0
ruoshuifuping <zhao2000fu@163.com>
2018-06-18 00:42:55 +0800

    Create proxy.py

---
+++ proxy.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+class ProxyClient():
+    
+    def restart_client(self):
+        while True:
+            (status, output) = subprocess.getstatusoutput('systemctl restart tinyproxy.service')
+            if status ==0:
+                print("tinyproxy 重启成功")
+                time.sleep(3600)
+            else:
+                print("tinyproxy 重启失败，再次重启")
+                
+def proxy():
+    client = ProxyClient()
+    client.restart_client()
+    
+if __name__ == '__main__':
+    proxy()
